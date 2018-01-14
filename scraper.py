3e6e9d3fe128d1ef38d8d279ccf03033a94bb09b
Daniel <danielrferriss@gmail.com>
2018-01-13 22:58:33 -0600

    Gets the recipe now

--- scraper.py
+++ scraper.py
@@ -1,9 +1,33 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-my_url = 'https://en.wikipedia.org/wiki/Randall_Road'
+
+#we will be scraping for mixed drink recipes
+my_url = 'https://www.liquor.com/recipes/page/1'
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
+#turn recipe 
 page_soup = soup(page_html, "html.parser")
 
-print(page_soup.prettify())
+for h3 in page_soup.find_all('h3'):
+	#Follows the link to the recipe
+	recipe_url = uReq(h3.a.get('href'))
+	recipe_html = recipe_url.read()
+	recipe_url.close()
+	#turn recipe page into soup
+	recipe_soup = soup(recipe_html, "html.parser")
+
+	print("\n")
+
+	amounts = recipe_soup.find("div", class_ = "ml-value")
+	ingredients = recipe_soup.find("div", class_ = "col-xs-8 x-recipe-ingredient")
+
+	#name of the drink
+	print(h3.get_text())
+	print("INGREDIENTS:")
+	print("RECIPE:")
+	recipe = recipe_soup.find("div", class_ = "row x-recipe-prep")
+	steps = recipe.find_all('p')
+	for text in steps:
+		print(text.get_text())
+
